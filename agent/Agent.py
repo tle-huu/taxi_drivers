297523ed2a59ef7611d29736dcfb6c937edf1a70
Terence Le Huu Phuong <tlehuu@hotmail.fr>
2020-02-16 17:30:22 +0100

    First agent try

---
+++ agent/Agent.py
@@ -0,0 +1,94 @@
+import numpy as np
+import torch
+from Memory import ReplayBuffer
+from DRL import CarLeader
+
+class Agent():
+    def __init__(self, gamma, epsilon, lr, n_actions, input_dims,
+                 mem_size, batch_size, eps_min=0.01, eps_dec=5e-7,
+                 replace=1000):
+        
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.lr = lr
+        self.n_actions = n_actions
+        self.input_dims = input_dims
+        self.eps_min = eps_min
+        self.eps_dec = eps_dec
+        self.action_space = list(range(n_actions))
+        self.learn_step_counter = 0
+        self.batch_size = batch_size
+        self.replace_target_cnt = replace
+
+        self.memory = ReplayBuffer(mem_size, input_dims, n_actions)
+
+    def store_transition(self, state, action, reward, state_, done):
+        self.memory.store_transition(state, action, reward, state_, done)
+
+    def choose_action(self, observation):
+        raise NotImplementedError
+
+    def replace_target_network(self):
+        if self.learn_step_counter % self.replace_target_cnt == 0:
+            self.q_next.load_state_dict(self.q_eval.state_dict())
+
+    def decrement_epsilon(self):
+        self.epsilon = self.epsilon - self.eps_dec \
+                           if self.epsilon > self.eps_min else self.eps_min
+    def sample_memory(self):
+        state, action, reward, new_state, done = \
+                                self.memory.sample_buffer(self.batch_size)
+
+        states = torch.tensor(state).to(self.q_eval.device)
+        rewards = torch.tensor(reward).to(self.q_eval.device)
+        dones = torch.tensor(done).to(self.q_eval.device)
+        actions = torch.tensor(action).to(self.q_eval.device)
+        states_ = torch.tensor(new_state).to(self.q_eval.device)
+
+        return states, actions, rewards, states_, dones
+
+    def learn(self):
+        raise NotImplementedError
+
+
+class DQNAgent(Agent):
+    def __init__(self, *args, **kwargs):
+        super(DQNAgent, self).__init__(*args, **kwargs)
+
+        self.q_eval = CarLeader(self.input_dims)
+        self.q_next = CarLeader(self.input_dims)
+
+    def choose_action(self, observation):
+        if np.random.random() > self.epsilon:
+            state = torch.tensor([observation],dtype=torch.float).to(self.q_eval.device)
+            actions = self.q_eval.forward(state)
+            action = torch.argmax(actions).item()
+        else:
+            action = np.random.choice(self.action_space)
+
+        return action
+
+    def learn(self):
+        if self.memory.mem_cntr < self.batch_size:
+            return
+
+        self.q_eval.optimizer.zero_grad()
+
+        self.replace_target_network()
+
+        states, actions, rewards, states_, dones = self.sample_memory()
+        indices = np.arange(self.batch_size)
+        print(states.unsqueeze(1))
+
+        q_pred = self.q_eval.forward(states.unsqueeze(1))[indices, actions]
+        q_next = self.q_next.forward(states_.unsqueeze(1)).max(dim=1)[0]
+        q_next[dones] = 0.0
+
+        q_target = rewards + self.gamma*q_next
+
+        loss = self.q_eval.loss(q_target, q_pred).to(self.q_eval.device)
+        loss.backward()
+        self.q_eval.optimizer.step()
+        self.learn_step_counter += 1
+
+        self.decrement_epsilon()
---
+++ agent/DRL.py
@@ -0,0 +1,61 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+def dimensions_after_conv(grid_shape, channels_number):
+    return channels_number * (grid_shape[0] * grid_shape[1])
+
+
+class CarLeader(nn.Module):
+
+    def __init__(self, grid_shape):
+
+        super(CarLeader, self).__init__()
+
+
+        self.convs1 = nn.Sequential(nn.Conv2d(1, 16, kernel_size = 3, padding = 1),
+                                    nn.BatchNorm2d(16),
+                                    nn.ReLU(True))
+
+        self.convs2 = nn.Sequential(nn.Conv2d(16, 32, kernel_size = 3, padding = 1),
+                                    nn.BatchNorm2d(32),
+                                    nn.ReLU(True))
+        
+        self.convs3 = nn.Sequential(nn.Conv2d(32, 64, kernel_size = 3, padding = 1),
+                                    nn.BatchNorm2d(64),
+                                    nn.ReLU(True))
+        
+        
+        self.flatten = torch.nn.Flatten()
+
+        features = int(dimensions_after_conv(grid_shape, 64))
+        self.fc1 = nn.Sequential(nn.Linear(features, 32), nn.ReLU(True))
+        self.fc2 = nn.Linear(32, 4)
+        self.loss = nn.MSELoss()
+        self.device = torch.device("cpu")
+        self.optimizer = optim.RMSprop(self.parameters(), lr = 0.01)
+        
+    def forward(self, x):
+        """
+        X is the grid dimension should be (n,1, gridshape[0], gridshape[1]) with n being
+        the number of grids in the batch
+        output is a vector of size (n,4) containing q values for up,down,left,right for 
+        each grid of the batch
+        """
+        x = self.convs1(x)
+        x = self.convs2(x)
+        x = self.convs3(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        return x
+"""
+Pour tester :
+grille = torch.rand(5,5).unsqueeze(0)
+grille = (grille<0.5).float()
+grille =grille.unsqueeze(0)
+
+traf = CarLeader([5,5])
+traf(grille)
+
+"""
